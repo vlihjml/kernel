5e7f93a91c7cedb4921f10a7bbc6619ff37eb543
Muflih Nurjamil <vlihjml@gmail.com>
2024-02-01 19:57:02 +0700

    Add files via upload

---
+++ build-ksu.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Copyright (C) 2023 psionicprjkt
+
+compile_kernel() {
+    # compile_kernel
+    export ARCH=arm64
+    make O=out ARCH=arm64 surya_defconfig
+    PATH="${PWD}/clang/bin:${PWD}/arm64:${PWD}/arm32:${PATH}" \
+    make -j"$(nproc --all)" O=out \
+        LLVM=1 \
+        LLVM_IAS=1 \
+        ARCH=arm64 \
+        CC="clang" \
+        LD=ld.lld \
+        STRIP=llvm-strip \
+        AS=llvm-as \
+        AR=llvm-ar \
+        NM=llvm-nm \
+        OBJCOPY=llvm-objcopy \
+        OBJDUMP=llvm-objdump \
+        CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE="${PWD}/arm64/aarch64-linux-android-" \
+        CROSS_COMPILE_COMPAT="${PWD}/arm32/arm-linux-androideabi-" \
+        CONFIG_NO_ERROR_ON_MISMATCH=y \
+        CFLAGS="-Wno-pragma-messages" 2>&1 | tee error.log
+}
+
+setup_kernel_release() {
+    # setup_kernel_release
+    git clone --depth=1 https://github.com/Konjikin/AnyKernel33  AnyKernel
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+    cp out/arch/arm64/boot/dtbo.img AnyKernel 
+    cd AnyKernel
+    zip -r9 "surya-$(date "+%d%m%Y")-release-ksu.zip" *
+}
+
+compile_kernel
+setup_kernel_release
---
+++ build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Copyright (C) 2023 psionicprjkt
+
+function psionic_compile()
+{
+    # cleanup_directories
+    rm -rf out AnyKernel AK3-* clang arm64 arm32 && mkdir -p out
+
+    # download_resources
+    git clone --depth=1 https://gitlab.com/LeCmnGend/proton-clang.git -b clang-13  clang
+
+
+  
+    # compile_kernel
+    export ARCH=arm64
+    make O=out ARCH=arm64 surya_defconfig
+    PATH="${PWD}/clang/bin:${PWD}/arm64:${PWD}/arm32:${PATH}" \
+        make -j$(nproc --all) O=out \
+        LLVM=1 \
+        LLVM_IAS=1 \
+        ARCH=arm64 \
+        CC="clang" \
+        LD=ld.lld \
+        STRIP=llvm-strip \
+        AS=llvm-as \
+        AR=llvm-ar \
+        NM=llvm-nm \
+        OBJCOPY=llvm-objcopy \
+        OBJDUMP=llvm-objdump \
+        CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE="${PWD}/arm64/aarch64-linux-android-" \
+        CROSS_COMPILE_COMPAT="${PWD}/arm32/arm-linux-androideabi-" \
+        CONFIG_NO_ERROR_ON_MISMATCH=y \
+        CFLAGS="-Wno-pragma-messages" 2>&1 | tee error.log
+}
+
+function psionic_upload()
+{
+    # setup_kernel_release
+    git clone --depth=1 https://github.com/Konjikin/AnyKernel33  AnyKernel
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+    cp out/arch/arm64/boot/dtbo.img AnyKernel
+    cd AnyKernel
+    zip -r9 "surya-$(date "+%d%m%Y")-release.zip" *
+
+    # upload_kernel_release
+    curl -s bashupload.com -T surya*
+}
+
+psionic_compile
+psionic_upload
